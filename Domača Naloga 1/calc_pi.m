68ab96e708a9374aa38ca87d8ef3b7ce7ed11873
JureZupancic <146855412+JureZupancic@users.noreply.github.com>
2023-10-21 17:01:00 +0200

    Izboljšana MATLAB koda

--- Domača Naloga 1/calc_pi.m
+++ Domača Naloga 1/calc_pi.m
@@ -1,10 +1,18 @@
-N = 1000
-mcc_pi(N);
+N = 10000;
+[x1_znotraj, y1_znotraj, x1_zunaj, y1_zunaj, znotraj]= mcc_pi(N);
+[pi_izrac, napaka] = area_pi(N, znotraj);
 
+% Izriše točke
+plot(x1_znotraj,y1_znotraj,'b.');
+hold on
+plot(x1_zunaj,y1_zunaj,'r.');
 
+% Izriše krožnico z radijem r
 fplot(@(t) 0.5*sin(t), @(t) 0.5*cos(t))
 
-function[NN] = area_pi(N)
-pi_izr = 4*sum(znotraj)/N
-napaka = abs(pi - pi_izr)
+% Funkcija, ki vse to omogoča
+function[pi_izr, napaka] = area_pi(N, znotraj)
+mcc_pi(N);
+pi_izr = 4*sum(znotraj)/N;
+napaka = abs(pi - pi_izr);
 end
--- Domača Naloga 1/mcc_pi.m
+++ Domača Naloga 1/mcc_pi.m
@@ -1,4 +1,4 @@
-function[nn] = mcc_pi(n)
+function[x1_znotraj, y1_znotraj, x1_zunaj, y1_zunaj, znotraj] = mcc_pi(n)
 % Najprej funkcija naključno ustvari koordinate točk x in y, glede na
 % vhodni parameter n, s katerim povemo koliko točk želimo
 x = rand(n,1);
@@ -9,18 +9,15 @@ y1=y-0.5;
 r = sqrt(x1.^2+y1.^2); 
 % Preko radija pa lahko določimo ali so točke zunaj krožnice ali znotraj
 % nje
-znotraj = r<=0.5; 
-zunaj = r>0.5; 
+znotraj = r<=0.5 ;
+zunaj = r>0.5 ;
 % Funkcija vrne x in y koordinate točk zunaj in znotraj krožnice v ločenih
 % arrayih
-x1_znotraj = x1(znotraj)
-y1_znotraj = y1(znotraj)
-x1_zunaj = x1(zunaj)
-y1_zunaj = y1(zunaj)
+x1_znotraj = x1(znotraj);
+y1_znotraj = y1(znotraj);
+x1_zunaj = x1(zunaj);
+y1_zunaj = y1(zunaj);
 
 
-plot(x1_znotraj,y1_znotraj,'b.');
-hold on
-plot(x1_zunaj,y1_zunaj,'r.');
 
 
